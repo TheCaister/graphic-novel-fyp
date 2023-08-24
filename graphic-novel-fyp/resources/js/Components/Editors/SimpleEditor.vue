b38d55f877b88df6bbfa05b2b5a06dd8a7ce2b0e
Edison <20241135@studentmail.ul.ie>
2023-08-24 15:03:19 +0100

    basic text editing functions

--- graphic-novel-fyp/resources/js/Components/Editors/SimpleEditor.vue
+++ graphic-novel-fyp/resources/js/Components/Editors/SimpleEditor.vue
@@ -1,5 +1,84 @@
 <template>
-    <editor-content :editor="editor" />
+
+    <div v-if="editor" class="flex flex-wrap space-x-2">
+        <button @click="editor.chain().focus().toggleBold().run()"
+            :disabled="!editor.can().chain().focus().toggleBold().run()"
+            :class="[editor.isActive('bold') ? 'is-active' : '', 'not-active']">
+            bold
+        </button>
+        <button @click="editor.chain().focus().toggleItalic().run()"
+            :disabled="!editor.can().chain().focus().toggleBold().run()"
+            :class="[editor.isActive('italic') ? 'is-active' : '', 'not-active']">
+            italic
+        </button>
+        <button @click="editor.chain().focus().toggleStrike().run()"
+            :disabled="!editor.can().chain().focus().toggleBold().run()"
+            :class="[editor.isActive('strike') ? 'is-active' : '', 'not-active']">
+            strike
+        </button>
+        <button @click="editor.chain().focus().unsetAllMarks().run()" class="not-active">
+            clear marks
+        </button>
+        <button @click="editor.chain().focus().clearNodes().run()" class="not-active">
+            clear nodes
+        </button>
+        <button @click="editor.chain().focus().setParagraph().run()"
+            :class="[editor.isActive('paragraph') ? 'is-active' : '', 'not-active']">
+            paragraph
+        </button>
+        <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+            :class="[editor.isActive('heading', { level: 1 }) ? 'is-active' : '', 'not-active']">
+            h1
+        </button>
+        <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+            :class="[editor.isActive('heading', { level: 2 }) ? 'is-active' : '', 'not-active']">
+            h2
+        </button>
+        <button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+            :class="[editor.isActive('heading', { level: 3 }) ? 'is-active' : '', 'not-active']">
+            h3
+        </button>
+        <button @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+            :class="[editor.isActive('heading', { level: 4 }) ? 'is-active' : '', 'not-active']">
+            h4
+        </button>
+        <button @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
+            :class="[editor.isActive('heading', { level: 5 }) ? 'is-active' : '', 'not-active']">
+            h5
+        </button>
+        <button @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
+            :class="[editor.isActive('heading', { level: 6 }) ? 'is-active' : '', 'not-active']">
+            h6
+        </button>
+        <button @click="editor.chain().focus().toggleBulletList().run(); removePfromList()"
+            :class="[editor.isActive('bulletList') ? 'is-active' : '', 'not-active']">
+            bullet list
+        </button>
+        <button @click="editor.chain().focus().toggleOrderedList().run(); removePfromList()"
+            :class="[editor.isActive('orderedList') ? 'is-active' : '', 'not-active']">
+            ordered list
+        </button>
+        <button @click="editor.chain().focus().toggleBlockquote().run()"
+            :class="[editor.isActive('blockquote') ? 'is-active' : '', 'not-active']">
+            blockquote
+        </button>
+        <button @click="editor.chain().focus().setHorizontalRule().run()" class="not-active">
+            horizontal rule
+        </button>
+        <button @click="editor.chain().focus().undo().run()" :disabled="!editor.can().chain().focus().undo().run()"
+            class="not-active">
+            undo
+        </button>
+        <button @click="editor.chain().focus().redo().run()" :disabled="!editor.can().chain().focus().redo().run()"
+            class="not-active">
+            redo
+        </button>
+    </div>
+
+    <div class="border-2 border-black">
+        <editor-content class="p-4 editor-field" :editor="editor" />
+
+    </div>
 </template>
   
 <script>
@@ -29,10 +108,10 @@ export default {
     watch: {
         modelValue(value) {
             // HTML
-            const isSame = this.editor.getHTML() === value
+            // const isSame = this.editor.getHTML() === value
 
             // JSON
-            // const isSame = JSON.stringify(this.editor.getJSON()) === JSON.stringify(value)
+            const isSame = JSON.stringify(this.editor.getJSON()) === JSON.stringify(value)
 
             if (isSame) {
                 return
@@ -42,21 +121,56 @@ export default {
         },
     },
 
+    methods: {
+        removePfromList() {
+            const newHTML = this.editor.getHTML().replaceAll(/<li><p>(.*?)<\/p><(\/?)(ol|li|ul)>/gi, "<li>$1<$2$3>")
+
+            this.editor.commands.setContent(newHTML, false)
+        }
+    },
+
     mounted() {
         this.editor = new Editor({
             extensions: [
-                StarterKit,
+                StarterKit.configure(
+                    {
+                        bulletList: {
+                            HTMLAttributes: {
+                                class: 'list-disc list-inside list',
+                            },
+                        },
+                        orderedList: {
+                            HTMLAttributes: {
+                                class: 'list-decimal list-inside list',
+                            },
+                        },
+                        blockquote: {
+                            HTMLAttributes: {
+                                class: 'border-l-4 border-gray-400 pl-4',
+                            },
+                        },
+                        horizontalRule: {
+                            HTMLAttributes: {
+                                class: 'border-0 border-t-2 border-gray-400 my-4',
+                            },
+                        },
+                    }
+                ),
             ],
             content: this.modelValue,
             onUpdate: () => {
+                // this.editor.commands.setContent(, false)
+
+                // console.log(this.editor.getHTML().replaceAll(/<li><p>(.*?)<\/p><(\/?)(ol|li|ul)>/gi, "<li>$1<$2$3>"))
                 // HTML
                 // Emit only the text in the editor, excluding the HTML tags
-                this.$emit('update:modelValue', this.editor.getHTML().replace(/<[^>]+>/g, ''))
-                
+                // this.$emit('update:modelValue', this.editor.getHTML().replace(/<[^>]+>/g, ''))
+
                 // this.$emit('update:modelValue', this.editor.getHTML())
 
                 // JSON
                 // this.$emit('update:modelValue', this.editor.getJSON())
+                // console.log(this.editor.getJSON());
             },
         })
     },
@@ -66,3 +180,53 @@ export default {
     },
 }
 </script>
+
+<style>
+ul > li {
+    margin-left: 20px;
+}
+
+ol > li {
+    margin-left: 20px;
+}
+
+.not-active {
+    background-color: #fff;
+    color: #000;
+
+    /* Add border with radius */
+    border: 1px solid #000;
+    border-radius: 5px;
+
+    padding: 5px 10px;
+}
+
+.is-active {
+    background-color: #000;
+    color: #fff;
+
+    /* Add border with radius */
+    border: 1px solid #000;
+    border-radius: 5px;
+
+    padding: 5px 10px;
+}
+
+button:disabled {
+    color: #a1a1a1;
+
+    /* Add border with radius */
+    border: 1px solid #a1a1a1;
+    border-radius: 5px;
+
+    padding: 5px 10px;
+}
+
+.list p {
+    display: inline;
+}
+
+.ProseMirror:focus {
+    outline: none;
+}
+</style>
--- graphic-novel-fyp/resources/js/Pages/Elements/Create.vue
+++ graphic-novel-fyp/resources/js/Pages/Elements/Create.vue
@@ -10,23 +10,50 @@
         <option value="timeline">Timeline</option>
     </select>
 
-    <component :is="selected">
-        <!-- Render the selected component -->
-    </component>
+    <div>
+        <div v-if="selected === 'Edit'">
+            <SimpleEditor v-model="form.content" />
+        </div>
+        <div v-else>
+            <p>Not implemented yet</p>
+        </div>
+    </div>
+
+
+    <!-- Make a button to go back and a button to create/save changes -->
+    <div>
+        <PrimaryButton>Back</PrimaryButton>
+        <PrimaryButton @click="submit()">>Save</PrimaryButton>
+    </div>
 </template>
 
 <script>
+import SimpleEditor from '@/Components/Editors/SimpleEditor.vue';
 import Edit from './SimpleElement/Edit.vue'
 
 export default {
     components: {
-        Edit
+        Edit,
+        SimpleEditor
     },
     data() {
         return {
-            selected: 'Edit'
+            selected: 'Edit',
+            form: {
+                title: '',
+                description: '',
+                image: '',
+                content: '',
+            }
         }
     },
+    methods: {
+        submit() {
+            // Submit the form
+            console.log(this.form);
+            this.form.content = '';
+        }
+    }
 }
 
 </script>
