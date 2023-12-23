3c61e057e1ca09deee16a3adf63804d639bd9ac3
Edison <20241135@studentmail.ul.ie>
2023-12-23 22:26:51 +0000

    getting started with vue teleport and modals

---
+++ graphic-novel-fyp/resources/js/Pages/Dashboard/CreateUniverseModal.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div class="bg-white">
+            <h2>Create Universe</h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
--- graphic-novel-fyp/resources/js/Pages/Dashboard/Universe.vue
+++ graphic-novel-fyp/resources/js/Pages/Dashboard/Universe.vue
@@ -1,58 +1,61 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import { defineProps } from 'vue';
+import CreateUniverseModal from './CreateUniverseModal.vue';
+import { ref } from 'vue'
 
 defineProps({
     name: {
         type: String,
     },
 });
+
+
+
+const universes = [
+    { id: 1, name: "Universe 1", description: "Description of Universe 1" },
+    { id: 2, name: "Universe 2", description: "Description of Universe 2" },
+    { id: 3, name: "Universe 3", description: "Description of Universe 3" },
+    // Add more universes as needed
+];
+
+// let isOpen = false;
+
+const isOpen = ref(false)
+
+// axios
+//     .get('/api/series/recent')
+//     .then((response) => (this.series = response.data))
+//     .catch((error) => console.log(error));
 </script>
 
 <template>
     <Head title="Dashboard" />
 
-    <div class="flex flex-wrap">
+    <div class="flex flex-wrap relative">
         <!-- Loop through the universes and display them in cards -->
         <Link v-for="universe in universes" :key="universe.id" class="bg-black rounded-lg shadow-md w-2/5 mx-8">
-            <div class="w-full h-64 bg-pink-300 flex items-center justify-center rounded-lg">
-                <img v-if="universe.image" :src="universe.image" alt="Universe Image" class="w-full h-full object-cover" />
-                <div v-else class="text-white text-xl">U{{ universe.id }}</div>
+        <div class="w-full h-64 bg-pink-300 flex items-center justify-center rounded-lg">
+            <img v-if="universe.image" :src="universe.image" alt="Universe Image" class="w-full h-full object-cover" />
+            <div v-else class="text-white text-xl">U{{ universe.id }}</div>
+        </div>
+        <p class="text-white pt-4">{{ universe.name }}</p>
+        </Link>
+
+        <button @click="isOpen = true" class="bg-black rounded-lg shadow-md w-2/5 mx-8">
+            <div class="w-full h-64 flex items-center justify-center rounded-lg">
+
+                <span class="material-symbols-outlined dark"
+                    style="font-size: 10rem; font-variation-settings: 'wght' 100; color: #f9a8d4;">
+                    add_circle
+                </span>
             </div>
-            <p class="text-white pt-4">{{ universe.name }}</p>
-        </Link>
+            <p class="text-white pt-4 text-center">Create Universe</p>
+        </button>
 
-        <Link class="bg-black rounded-lg shadow-md w-2/5 mx-8">
-                <div class="w-full h-64 flex items-center justify-center rounded-lg">
-
-                    <span class="material-symbols-outlined dark"
-                        style="font-size: 10rem; font-variation-settings: 'wght' 100; color: #f9a8d4;">
-                        add_circle
-                    </span>
-                </div>
-                <p class="text-white pt-4 text-center">Create Universe</p>
-        </Link>
+        <Teleport to="body">
+            <create-universe-modal v-if="isOpen" @close="isOpen = false" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60"/>
+        </Teleport>
 
     </div>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            // universes: [],
-            universes: [
-                { id: 1, name: "Universe 1", description: "Description of Universe 1" },
-                { id: 2, name: "Universe 2", description: "Description of Universe 2" },
-                { id: 3, name: "Universe 3", description: "Description of Universe 3" },
-                // Add more universes as needed
-            ],
-        };
-    },
-    mounted() {
-        // axios
-        //     .get('/api/series/recent')
-        //     .then((response) => (this.series = response.data))
-        //     .catch((error) => console.log(error));
-    },
-};
-</script>
