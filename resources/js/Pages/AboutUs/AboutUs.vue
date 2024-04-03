c2a3553c74319ed85d180109cef0aa9fdb2acbe4
Edison <20241135@studentmail.ul.ie>
2024-04-03 18:39:22 +0100

    button for survey

--- resources/js/Pages/AboutUs/AboutUs.vue
+++ resources/js/Pages/AboutUs/AboutUs.vue
@@ -1,13 +1,22 @@
 <template>
 
-    <div class="text-white text-lg">
-        Welcome to Comic Forge!
-        In this web application, you'll be able to plan your own comic/manga series!
-        Developed by Edison Cai
+    <div class="text-white text-2xl p-16">
+        <div>
+            Welcome to Comic Forge!
+            In this web application, you'll be able to plan your own comic/manga series!
+            Developed by Edison Cai
+        </div>
+
+        <a href="https://forms.office.com/e/swJ80EPVA8" target="_blank">
+            <PrimaryButton>
+                Fill out the survey
+            </PrimaryButton>
+        </a>
     </div>
 
 </template>
 
 <script setup>
+
 
 </script>
